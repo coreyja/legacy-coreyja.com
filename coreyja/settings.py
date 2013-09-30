62801f5d834e01f83c3415cc877c66463fc10f01
Corey <coreyja@gmail.com>
2013-09-30 05:47:54 -0400

    Changed base.html to include minified css if not in debug mode. Had to set internal_ips in local_settings for this to work

--- coreyja/settings.py
+++ coreyja/settings.py
@@ -2,7 +2,7 @@
 import os.path
 PROJECT_DIR = os.path.dirname(__file__)
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -13,10 +13,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'coreyja',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'coreyja',
+        'USER': '',
         'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
@@ -163,3 +163,11 @@ LOGGING = {
         },
     }
 }
+
+###############################################################################
+# Import local settings (override)
+###############################################################################
+try:
+    from local_settings import *
+except ImportError:
+    pass
