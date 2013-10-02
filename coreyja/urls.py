041d47274b41a662f65e4dbde655d7a6e8b131cd
Corey <coreyja@gmail.com>
2013-10-02 12:53:41 -0400

    Added resume files

--- coreyja/urls.py
+++ coreyja/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 from main.views import HomeView, ProjectView
+
 import settings
 
 # Uncomment the next two lines to enable the admin:
@@ -16,6 +18,7 @@ urlpatterns = patterns('',
     url(r'^(?P<page>\d+)/$', HomeView.as_view(), name="home_paginated"),
 
     url(r'^project/(?P<slug>\w+)/$', ProjectView.as_view(), name="project"),
+    url(r'^resume/$', TemplateView.as_view(template_name="resume.html"), name="project"),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
